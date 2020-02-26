d3677f6f978e6f2a6a40a665abdeb197854b9598
razmanov666 <razmanov666@gmail.com>
2020-02-26 23:29:54 +0300

    refactor: cosmetic change code

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 
 exports.min = function min (array) {
-  let res = 0;
-  Math.min.apply(Math,array) == 'Infinity' ? res = 0 : res =  Math.min.apply(Math,array);
+  let res = [], min = Math.min.apply(Math,array);
+  if (min === Infinity) 
+    return res; 
+  else 
+    res = min;
   return res;
 }
 
 exports.max = function max (array) {
-  let res = 0;
-  console.log(Math.max.apply(Math,array));
-  Math.max.apply(Math,array) == 'Infinity' || Math.max.apply(Math,array) == '-Infinity' ? res = 0 : res =  Math.max.apply(Math,array);
+  let res = [], max=Math.max.apply(Math,array);
+  if (max === Infinity || 
+    max === -Infinity )
+    return res;
+  else 
+    res = max;
   return res;
 }
 
 exports.avg = function avg (array) {
-  let sum = 0;
-  let res = 0;
-  Math.min.apply(Math,array) == 'Infinity' || array == 'undefined' ? doNothing(): array.forEach(element => {sum+=element});
-  Math.min.apply(Math,array) == 'Infinity' ? res = 0 : res = sum/array.length;
+  let sum = 0, res = [];
+  if (array === undefined ||
+    array.length === 0)
+    return res;
+  else 
+    array.forEach(element => {sum+=element});
+    res = sum/array.length;
   return res;
 }
-function doNothing(){
-}
-/*
-let arr=[13,30,32,-25,39,32,31,-34,15,-20,-10,27,24,-9,24,-30,-25,-7,30,10];
-console.log(max(arr));
-*/
